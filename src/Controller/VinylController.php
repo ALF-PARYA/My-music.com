f9dc84c3002d557dbe5c9f26ff7245850a0bbe7a
parya <antoine020999@gmail.com>
2024-01-22 17:19:20 +0100

    premier commit

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -1,3 +1,25 @@
-<? 
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
+class VinylController
+{
+    #[Route('/')]
+    public function homepage()
+    {
+        return new Response('Title: "PB and Jams"');
+    }
 
-$init = "init";
+    #[Route('/browse/{slug}')]
+    public function browse( string $slug = null): Response 
+    {
+        if($slug){
+            $title = 'Genre'.u(str_replace('-', ' ', $slug))->title(true);
+        }else{
+            $title = 'Tous les genres';
+        }
+            return new Response($title);
+    }
+
+}
