a1e9d81fec13a7552559c1bcdda26e7c1449e564
parya <antoine020999@gmail.com>
2024-01-24 13:45:11 +0100

    ajout de twigg

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -1,14 +1,28 @@
 <?php
 namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
-class VinylController
+
+class VinylController extends AbstractController
 {
     #[Route('/')]
     public function homepage()
     {
-        return new Response('Title: "PB and Jams"');
+        $tracks = [
+            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['song' => 'Waterfalls', 'artist' => 'TLC'],
+            ['song' => 'Creep', 'artist' => 'Radiohead'],
+            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+        
+        return $this->render('vinyl/homepage.html.twig',[
+        'title' => 'Liste des musiques',
+        'tracks' => $tracks ,
+        ]);
     }
 
     #[Route('/browse/{slug}')]
@@ -20,6 +34,7 @@ class VinylController
             $title = 'Tous les genres';
         }
             return new Response($title);
+
     }
 
 }
